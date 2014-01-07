e889df794e834667ec6d00959d33055be1cad380
Elijah Insua <tmpvar@gmail.com>
2014-01-06 22:12:49 -0700

    initial

---
+++ Assets/GUI/guiToggle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class guiToggle : MonoBehaviour {
+	//[SerializeField] GameObject Active;
+	[SerializeField] GameObject Passive;
+	bool toggle = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+	void OnMouseOver() {
+
+	}
+	void onMouseEnter(){Debug.Log("you are on me");}
+	void onMouseExit(){Debug.Log ("agh!");}
+	void onMouseOver(){Debug.Log("MONKEY");}
+}
