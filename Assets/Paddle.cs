9bdaafc117791ed2f67ecf9eac5e368b46dcf0b9
Elijah Insua <tmpvar@gmail.com>
2014-01-07 19:41:02 -0700

    add arena

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -10,6 +10,20 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey("left")) {
+			this.transform.position = new Vector3(
+				this.transform.position.x,
+				this.transform.position.y,
+				this.transform.position.z+ 0.1f
+				);
+		}
+
+		if (Input.GetKey("right")) {
+			this.transform.position = new Vector3(
+				this.transform.position.x,
+				this.transform.position.y,
+				this.transform.position.z - 0.1f
+				);
+		}
 	}
 }
