e9866a30a81ac52188562f14b7c475debc0ba94e
Richard H <blendblob@gmail.com>
2014-01-09 00:20:33 -0700

    0-999 number with interface

--- Assets/GUI/guiNumberController.cs
+++ Assets/GUI/guiNumberController.cs
@@ -3,24 +3,26 @@ using System.Collections;
 
 public class guiNumberController : MonoBehaviour {
 	[SerializeField] GameObject guiNum;
-	[SerializeField] float spacing = 0.000006f;
+	float spacing = 0.06f;
 	GameObject	newnum_Ones;
 	GameObject	newnum_Tens;
 	GameObject	newnum_Hundreds;
 	int number;
+	public void setNumber(int a){number = a;}
+	public int getNumber(){return number;}
 	void Start () {
 		//+Vector3(spacing*1f,0f,0f)
 		newnum_Ones = (GameObject) Instantiate(guiNum, transform.position, transform.rotation);
 		newnum_Ones.transform.parent = this.transform;
 		//newnum_Ones.transform.parent = this.gameObject;
 		newnum_Tens = (GameObject) Instantiate(guiNum, transform.position, transform.rotation);
-		newnum_Tens.transform.Translate = new Vector3 (0.1f, 0f, 0f);
-		//newnum_Ones.transform.parent = this.transform;
+		newnum_Tens.transform.Translate(spacing*1f, 0f, 0f);
+		newnum_Tens.transform.parent = this.transform;
 
 		//newnum_Tens.transform.parent = this.gameObject;
 		newnum_Hundreds = (GameObject) Instantiate(guiNum, transform.position, transform.rotation);
-		newnum_Tens.transform.Translate = new Vector3 (0.1f, 0f, 0f);
-		//newnum_Ones.transform.parent = this.transform;
+		newnum_Hundreds.transform.Translate(spacing*2f, 0f, 0f);
+		newnum_Hundreds.transform.parent = this.transform;
 		//newnum_Hundreds.transform.parent = this.gameObject;
 	}
 	void Update () {
@@ -28,8 +30,6 @@ public class guiNumberController : MonoBehaviour {
 		newnum_Ones.GetComponent<guiNumberv2> ().setHidden(true);
 		newnum_Tens.GetComponent<guiNumberv2> ().setHidden(true);
 		newnum_Hundreds.GetComponent<guiNumberv2> ().setHidden(true);
-
-		number = (int )Time.time;
 		int a = number % 10;
 		if (a >= 0) {
 			newnum_Ones.GetComponent<guiNumberv2> ().setNumber(a);
