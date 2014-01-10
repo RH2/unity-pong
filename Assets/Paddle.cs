ad60fce1ac7558457967f92cdbb4a29a3eff6e33
Elijah Insua <tmpvar@gmail.com>
2014-01-10 03:42:45 -0700

    space to shoot the ball

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
+	public Vector3 velocity;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,19 +13,26 @@ public class Paddle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey("left")) {
-			this.transform.position = new Vector3(
-				this.transform.position.x,
-				this.transform.position.y,
-				this.transform.position.z+ 0.1f
-				);
+			this.velocity += new Vector3(0.0f, 0.0f, 10.0f);
 		}
 
 		if (Input.GetKey("right")) {
-			this.transform.position = new Vector3(
-				this.transform.position.x,
-				this.transform.position.y,
-				this.transform.position.z - 0.1f
-				);
+			this.velocity -= new Vector3(0.0f, 0.0f, 10.0f);
 		}
+
+		if (Input.GetKeyUp("space")) {
+			GameObject [] balls = GameObject.FindGameObjectsWithTag("ball");
+			foreach (GameObject b in balls) {
+				Debug.Log ("here");
+				if (b.transform.parent == this.transform) {
+					b.transform.parent = null;
+					b.rigidbody.velocity = velocity + new Vector3((transform.position.x < 0) ? 50.0f : -50.0f, 0.0f, 0.0f);
+				}
+			}
+
+		};
+
+		this.transform.position += velocity * Time.deltaTime;
+		velocity.Scale(new Vector3(0.0f, 0.0f, 0.8f));
 	}
 }
